83a573cc27ad220281cd45712d5a84b2a91bcefb
Tommaso Buonocore <buonocore.tms@gmail.com>
2021-07-26 17:16:59 +0200

    major refactoring: dynamic html generation

---
+++ data/data.js
@@ -0,0 +1,297 @@
+var data = [
+  {
+    type: "token",
+    title: "2021"
+  },
+
+ {
+    type: "job",
+    title: "Ph.D. Student",
+    subtitle: "Big Data & Biomedical Informatics",
+    description: "Currently&#32;researching&#32;NLP&#32;techniques&#32;to&#32;improve&#32;patient&#32;representation&#32;learning&#32;and&#32;classification&#32;with&#32;italian&#32;biomedical&#32;language&#32;models.&#32;",
+    topics: ["NLP","TopicModeling","LanguageModels"],
+    location: {
+      place:"Pavia",
+      link: "https://goo.gl/maps/JkFHxR87TyY2Bc95A"
+    },
+    date: {
+      start: "01/10/2020",
+      end: "ongoing"
+    },
+    image: "unipv.png"
+  },
+
+  {
+    type: "paper",
+    title: "Exploring&#32;chromatic&#32;storytelling&#32;in&#32;movies&#32;with&#32;R:&#32;color&#32;palette&#32;extraction",
+    subtitle: "Sep 19, 2020 - Towards Data Science",
+    description: "Instead of focusing on sequences of colors and chromatic patterns across entire movies, the cornerstone of this third part will be the visual inspection of a single frame. Specifically, our main objective will be the automatic generation of proper color palettes from any input frame using unsupervised machine learning techniques and color quantization theory.<br>",
+    topics: [],
+    date: {
+      start: "19/09/2020",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/r-color-palette-extraction-3850b3f56152"
+  },
+
+  {
+    type: "project",
+    title: "Sinapsi",
+    subtitle: "May, 2020 - Project",
+    description: "Sinapsi is a web application designed to provide an easy connection between italian family doctors and psychologists, dispensing also useful informative content.<br> Sinapsi's services are meant to be used from trusted family doctors only and are therefore password-protected using a clinet-side AES-256 encryption.<br> A password-free demo version is available for illustrative purposes <a href='https://detsutut.github.io/sinapsi/demo' style='color: #fff;'><strong>here</strong></a>.",
+    topics: [],
+    date: {
+      start: "15/05/2020",
+      end: null
+    },
+    footer: "https://detsutut.github.io/sinapsi/demo"
+  },
+
+  {
+    type: "project",
+    title: "Fantasimulatore",
+    subtitle: "May, 2020 - Project",
+    description: "Shiny web app to simulate alternative realities and cancel out the luck factor from Italian Serie A fantasy football (a.k.a. Fantacalcio). <a href='https://detsutut.github.io/sinapsi/demo' style='color: #fff;'><strong>Click here to give it a try!</strong></a>.",
+    topics: [],
+    date: {
+      start: "01/05/2020",
+      end: null
+    },
+    footer: "https://github.com/detsutut/fantaSimulatore"
+  },
+
+  {
+    type: "paper",
+    title: "Is COVID-19 Lockdown Cleaning the Skies Over Milan?",
+    subtitle: "Apr 12, 2020 - Towards Data Science",
+    description: "It’s been more than one month since the Italian government adopted the most sweeping measures outside China to face the Coronavirus outbreak, locking down the most populous, richest and most productive region of the Bel Paese: Lombardy. Consequential travel bans and the shutdown of all non-essential businesses and industries had a huge impact on Italy’s fragile economy, but also gave researchers a unique chance to collect data and study the environment we live in under never-seen-before conditions.",
+    topics: [],
+    date: {
+      start: "12/04/2020",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/is-covid-19-lockdown-cleaning-the-skies-over-milan-42dbba1ec812"
+  },
+
+  {
+    type: "paper",
+    title: "Deep Learning & Healthcare: All That Glitters Ain’t Gold",
+    subtitle: "Feb 28, 2020 - Towards Data Science",
+    description: "An overview of the complex relationship between Deep Learning and Healthcare, discussing the main reasons why healthcare providers should dwell more on who may really benefit from this new approach.",
+    topics: [],
+    date: {
+      start: "28/02/2020",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/deep-learning-in-healthcare-all-the-glitters-aint-gold-4913eec32687#91a1-40e489267686"
+  },
+
+  {
+    type: "special",
+    content: "<span class='timeline-virus tilt' onclick='checkClicks()'> <br><br><br> <img src='virus.png' id='virus' class='img-fluid rounded-circle' alt='...' style='width:15%; cursor: pointer;'> <br><br><br><br> </span> <!-- Modal --> <div class='modal fade' style='opacity:0.9;' id='coffinDanceModal' tabindex='-1' role='dialog' aria-labelledby='coffinDanceModal' aria-hidden='true'> <div class='modal-dialog modal-dialog-centered' role='document'> <div class='modal-content'> <div class='embed-responsive embed-responsive-16by9'> <iframe class='embed-responsive-item' src='https://www.youtube.com/embed/pDmB76y7jFs' allowfullscreen></iframe> </div> </div> </div> </div>",
+  },
+
+  {
+    type: "token",
+    title: "2020"
+  },
+
+  {
+    type: "paper",
+    title: "All I want for Christmas is AI: write the next Christmas hit using LSTMs",
+    subtitle: "Dec 30, 2019 - Towards Data Science",
+    description: "Generating text with AI is one of the most complete and satisfying tasks a Data Scientist can work on. Given the time of the year and the predictable nature of Christmas carols, here we try to generate Christmas lyrics automatically using Long Short Term Memory recurrent neural networks.",
+    topics: [],
+    date: {
+      start: "30/12/2019",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/all-i-want-for-christmas-is-ai-write-the-next-christmas-hit-using-lstms-2e95ea3da25e"
+  },
+
+  {
+    type: "project",
+    title: "ShinyDBNet",
+    subtitle: "Nov, 2019 - Project",
+    description: "Originally developed whithin the AMC's ADFICE_IT project as an educational tool for fall-risk prevention in eldery patients, this open-source Shiny application evolved as a public, generic toolkit providing an interactive framework for learning, visualizing and reasoning with Discrete Bayesian Networks (DBNs). A DBN is a type of probabilistic graphical model that uses Bayesian inference for probability computations.<br><a href='https://detsutut.shinyapps.io/shinyDBNet/' style='color: #fff;'><strong>Click here to run the web app</strong></a>.",
+    topics: [],
+    date: {
+      start: "01/11/2019",
+      end: null
+    },
+    footer: "https://github.com/detsutut/shinyDBNet"
+  },
+
+  {
+    type: "paper",
+    title: "Exploring chromatic storytelling in movies with R: the ChromaR package",
+    subtitle: "Aug 5, 2019 - Towards Data Science",
+    description: "Color can affect us psychologically and emotionally without us even becoming aware. Different colors can be used to create harmony either tension within a scene, to reinforce an idea the director is trying to convey to the spectators, to bring attention to a key visual theme, to show a character’s journey, and more. ChromaR is a R package which allows the user to explore many aspects of chromatic visualizations.",
+    topics: [],
+    date: {
+      start: "05/08/2019",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/the-chromar-package-892b716ee2c9"
+  },
+
+  {
+    type: "job",
+    title: "Machine Learning Engineer",
+    subtitle: "Academic Medical Center (AMC)",
+    description: "Worked on falls prevention in eldery patients, implementing a support system based on discrete bayesian networks. Researched Natural Language Processing methods (word embeddings, language models) applied to free-text clinical notes.",
+    topics: ["NLP","Machine Learning","Bayesian Networks"],
+    location: {
+      place:"Amsterdam",
+      link: "https://g.page/Amsterdam_UMC_loc_AMC?share"
+    },
+    date: {
+      start: "Aug 2019",
+      end: "Mar 2020"
+    },
+    image: "umc.png"
+  },
+
+  {
+    type:"special",
+    content:"<span class='timeline-virus tilt' data-toggle='modal' data-target='#exampleModalCenter'> <br> <img src='grad.png' class='img-fluid rounded-circle ' alt='...' style='cursor: pointer; width:15%'> <br><br><br><br> </span> <!-- Modal --> <div class='modal fade' style='opacity:0.9;' id='exampleModalCenter' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'> <div class='modal-dialog modal-dialog-centered' role='document'> <div class='modal-content'> <div class='modal-header'> <h5 class='modal-title' id='exampleModalLongTitle'>Master's Degree in Biomedical Engineering</h5><br> <button type='button' class='close' data-dismiss='modal' aria-label='Close'> <span aria-hidden='true'>&times;</span> </button> </div> <div class='modal-body'> <h6>University of Pavia, 110 / 110 Cum Laude</h6> <p> Period: Sep 2016 - Jul 2019 </p> <p> Courses: <ul> <li>Advanced Biomedical Machine Learning (30/30)</li> <li>Medical Informatics (30L/30)</li> <li>Data Mining and Clinical Decision Support (30/30)</li> <li>Telemedicine (29/30)</li> <li>Information Security (30/30)</li> <li>Biomathematics (28/30)</li> <li>Artificial Intelligence in Medicine (28/30)</li> </ul> </p> <p>Link: <a href='http://lmbioing.unipv.eu/' target='_blank'> Biomedical Engineering official website</a></p> </div> <div class='modal-footer'> <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button> </div> </div> </div> </div>"
+  },
+
+  {
+    type: "job",
+    title: "Software Engineer",
+    subtitle: "Biomeris S.r.l.",
+    description: "Worked on software modules for Diagnostic Therapeutic Care Pathway management for Multiple Sclerosis.",
+    topics: ["JavaScript","Angular","Health Informatics"],
+    location: {
+      place:"Pavia",
+      link: "https://goo.gl/maps/JkFHxR87TyY2Bc95A"
+    },
+    date: {
+      start: "May 2019",
+      end: "Jul 2019"
+    },
+    image: "biomeris.png"
+  },
+
+  {
+    type: "paper",
+    title: "Exploring chromatic storytelling in movies with R",
+    subtitle: "Apr 8, 2019 - Towards Data Science",
+    description: "Color is one of the most powerful means of conveying information to the audience, and this is why many of the greatest movie directors have an extensive background in visual arts. Data can be used to investigate their chromatic fingerprints.",
+    topics: [],
+    date: {
+      start: "08/04/2019",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/exploring-chromatic-storytelling-with-r-part-1-8e9ddf8d4187"
+  },
+
+  {
+    type: "project",
+    title: "ChromaR",
+    subtitle: "Apr, 2019 - Project",
+    description: "ChromaR is a R package that leverages color theory, data visualization and machine learning to provide a wide variety of visual and analytical tools for chromatic analysis of movies and clips. ChromaR can be also used to extract color palettes from stills and pictures.",
+    topics: [],
+    date: {
+      start: "05/04/2019",
+      end: null
+    },
+    footer: "https://github.com/detsutut/chroma"
+  },
+
+  {
+    type: "paper",
+    title: "Man is to Doctor as Woman is to Nurse: the Dangerous Bias of Word Embeddings",
+    subtitle: "Mar 8, 2019 - Towards Data Science",
+    description: "Language is one of the most powerful means through which sexism and gender discrimination are perpetrated and reproduced, and a naive application of NLP techniques risks to transfer these social biases risk to machines.",
+    topics: [],
+    date: {
+      start: "08/03/2019",
+      end: null
+    },
+    footer: "https://towardsdatascience.com/gender-bias-word-embeddings-76d9806a0e17"
+  },
+
+  {
+    type: "job",
+    title: "Author",
+    subtitle: "Towards Data Science",
+    description: "Writing about Data Science, Machine Learning, Artificial Intelligence and Programming for one of the most popular data science blogs in the world.",
+    topics: ["Data Science","Technical Writing","Education"],
+    location: {
+      place:"Online",
+      link: ""
+    },
+    date: {
+      start: "Mar 2019",
+      end: "Ongoing"
+    },
+    image: "tds.png"
+  },
+
+  {
+    type: "token",
+    title: "2019"
+  },
+
+  {
+    type: "job",
+    title: "Machine Learning Engineer",
+    subtitle: "Academic Medical Center (AMC)",
+    description: "Researched NLP models for the early detection of Colorectal Cancer leveraging EHR unstructured data.",
+    topics: ["NLP","Machine Learning","R","Python"],
+    location: {
+      place:"Amsterdam",
+      link: "https://g.page/Amsterdam_UMC_loc_AMC?share"
+    },
+    date: {
+      start: "Oct 2018",
+      end: "Mar 2019"
+    },
+    image: "umc.png"
+  },
+
+  {
+    type: "token",
+    title: "2018"
+  },
+
+  {
+    type: "token",
+    title: "2017"
+  },
+
+  {
+    type: "job",
+    title: "Software Engineer",
+    subtitle: "Biomeris S.r.l.",
+    description: "Worked on a JavaScript library for genograms representation and visualization.",
+    topics: ["JavaScript","NoSQL Database","Health Informatics"],
+    location: {
+      place:"Pavia",
+      link: "https://goo.gl/maps/JkFHxR87TyY2Bc95A"
+    },
+    date: {
+      start: "Apr 2016",
+      end: "Sep 2016"
+    },
+    image: "umc.png"
+  },
+
+  {
+    type: "special",
+    content: "<span class='timeline-virus tilt' data-toggle='modal' data-target='#modalBachelor'> <br> <img src='grad.png' class='img-fluid rounded-circle ' alt='...' style='cursor: pointer; width:15%'> <br><br><br><br> </span> <!-- Modal --> <div class='modal fade' style='opacity:0.9;' id='modalBachelor' tabindex='-1' role='dialog' aria-labelledby='modalBachelorTitle' aria-hidden='true'> <div class='modal-dialog modal-dialog-centered' role='document'> <div class='modal-content'> <div class='modal-header'> <h5 class='modal-title' id='modalBachelorTitle'>Bachelor's Degree in Biomedical Engineering</h5><br> <button type='button' class='close' data-dismiss='modal' aria-label='Close'> <span aria-hidden='true'>&times;</span> </button> </div> <div class='modal-body'> <h6>University of Pavia, 100 / 110 </h6> <p> Period: Sep 2012 - Apr 2016 </p> <p> Courses: <ul> <li>Advanced Biomedical Machine Learning (30/30)</li> <li>Medical Informatics (30L/30)</li> <li>Data Mining and Clinical Decision Support (30/30)</li> <li>Telemedicine (29/30)</li> <li>Information Security (30/30)</li> <li>Biomathematics (28/30)</li> <li>Artificial Intelligence in Medicine (28/30)</li> </ul> </p> <p>Link: <a href='http://lmbioing.unipv.eu/' target='_blank'> Biomedical Engineering official website</a></p> </div> <div class='modal-footer'> <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button> </div> </div> </div> </div>"
+  },
+
+  {
+    type: "token",
+    title: "2016"
+  },
+
+  {
+    type: "special",
+    content: "<span class='timeline-label'> <span class='label bg-primary'>06 / 06 / 1993</span> </span>"
+  }
+
+]
